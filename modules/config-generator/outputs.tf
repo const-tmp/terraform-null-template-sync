9530dda84a591bcd9e154b0a7f5a39dc87fadc4a
anon <anon@local>
2023-03-16 19:12:28 +0200

    fix config-generator output name

--- modules/config-generator/outputs.tf
+++ modules/config-generator/outputs.tf
@@ -1,4 +1,4 @@
-output "config_new" {
+output "config" {
   description = "Generated config"
   value       = merge([for config_name, labels in var.input : {
     for label, data in labels : "${config_name}-${label}" => data.config
